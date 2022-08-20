393cbead00886a7f736322eac5330270fbdf4d43
Sotthiporn <sotthiporndeveloper@gmail.com>
2022-08-21 00:46:47 +0700

    pm2

--- pages/index.vue
+++ pages/index.vue
@@ -11,8 +11,8 @@
                 slide.image
               "
               alt="slide"
-              width="50"
-              height="50"
+              width="100"
+              height="100"
             />
           </div>
         </template>
